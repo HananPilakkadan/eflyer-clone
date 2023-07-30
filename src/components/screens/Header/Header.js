8e2ce6e901638f0d0b2c6d686c8a76f72dda645a
ajeeb <hananpilakkadan@gmail.com>
2023-07-30 15:26:22 +0530

    responsive screens

--- src/components/screens/Header/Header.js
+++ src/components/screens/Header/Header.js
@@ -69,91 +69,98 @@ const Header = () => {
             </h1>
           </div>
           <div className="bottom-header">
-            <div className="hamburger header-item" onClick={handleHamburger}>
-              <img
-                src={require("../../assets/image/hamburger.png")}
-                alt="Hamburger"
-              />
-              <div
-                className={
-                  hamburger ? "hamburger-menu" : "hamburger-menu active"
-                }
-              >
-                <div className="close">
-                  <span onClick={closeHamburger}>
-                    <CloseOutlined />
+            <div className="left-box">
+              <div className="hamburger header-item" onClick={handleHamburger}>
+                <img
+                  src={require("../../assets/image/hamburger.png")}
+                  alt="Hamburger"
+                />
+                <div
+                  className={
+                    hamburger ? "hamburger-menu" : "hamburger-menu active"
+                  }
+                >
+                  <div className="close">
+                    <span onClick={closeHamburger}>
+                      <CloseOutlined />
+                    </span>
+                  </div>
+                  <div className="content">
+                    {category?.map((item, key) => {
+                      return (
+                        <>
+                          <span key={key}>
+                            <a href="#">{item}</a>
+                          </span>
+                        </>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="header-item category" onClick={handleCategory}>
+                <button>
+                  <span>All categories</span>
+                  <CaretDownOutlined />
+                </button>
+                {categoryDropDown && (
+                  <div className="drop-down">
+                    {category?.map((item, key) => {
+                      return (
+                        <>
+                          <span key={key}>
+                            <a href="#">{item}</a>
+                          </span>
+                        </>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+              <div className="header-item form-box">
+                <form action="#">
+                  <input type="text" placeholder="Search this blog" />
+                  <button>
+                    <SearchOutlined />
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="right-box">
+              <div className="header-item languages" onClick={handleLang}>
+                <a href="#" className="language">
+                  <img
+                    src={require("../../assets/image/flag-uk.png")}
+                    alt="UK"
+                  />
+                  <span>English</span>
+                  <DownOutlined />
+                </a>
+                {langDropDown && (
+                  <div className="drop-down">
+                    <a href="#">
+                      <img
+                        src={require("../../assets/image/flag-france.png")}
+                        alt="UK"
+                      />
+                      <span>French</span>
+                    </a>
+                  </div>
+                )}
+              </div>
+              <div className="header-item user">
+                <div className="user-item">
+                  <span>
+                    <ShoppingCartOutlined />
+                    Cart
                   </span>
                 </div>
-                <div className="content">
-                  {category?.map((item, key) => {
-                    return (
-                      <>
-                        <span key={key}>
-                          <a href="#">{item}</a>
-                        </span>
-                      </>
-                    );
-                  })}
+                <div className="user-item">
+                  <span>
+                    <UserOutlined />
+                    Cart
+                  </span>
                 </div>
-              </div>
-            </div>
-            <div className="header-item category" onClick={handleCategory}>
-              <button>
-                <span>All categories</span>
-                <CaretDownOutlined />
-              </button>
-              {categoryDropDown && (
-                <div className="drop-down">
-                  {category?.map((item, key) => {
-                    return (
-                      <>
-                        <span key={key}>
-                          <a href="#">{item}</a>
-                        </span>
-                      </>
-                    );
-                  })}
-                </div>
-              )}
-            </div>
-            <div className="header-item form-box">
-              <form action="#">
-                <input type="text" placeholder="Search this blog" />
-                <button>
-                  <SearchOutlined />
-                </button>
-              </form>
-            </div>
-            <div className="header-item languages" onClick={handleLang}>
-              <a href="#" className="language">
-                <img src={require("../../assets/image/flag-uk.png")} alt="UK" />
-                <span>English</span>
-                <DownOutlined />
-              </a>
-              {langDropDown && (
-                <div className="drop-down">
-                  <a href="#">
-                    <img
-                      src={require("../../assets/image/flag-france.png")}
-                      alt="UK"
-                    />
-                    <span>French</span>
-                  </a>
-                </div>
-              )}
-            </div>
-            <div className="header-item user">
-              <div className="user-item">
-                <span>
-                  <ShoppingCartOutlined />
-                  Cart
-                </span>
-              </div>
-              <div className="user-item">
-                <span>
-                  <UserOutlined />
-                  Cart
-                </span>
               </div>
             </div>
           </div>
